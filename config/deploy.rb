5935944d25bd5394e6982446bada6bb42783dea7
Eric Roos <ericroos13@gmail.com>
2014-11-03 21:25:17 -0600

    maybe?

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,10 +28,10 @@ set :user, 'eric'    # Username in the server to SSH to.
 task :environment do
   # If you're using rbenv, use this to load the rbenv environment.
   # Be sure to commit your .rbenv-version to your repository.
-  # invoke :'rbenv:load'
+  #invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  invoke :'rvm:use[ruby-1.9.3-p125@default]'
+  #invoke :'rvm:use[ruby-1.9.3-p125@default]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
