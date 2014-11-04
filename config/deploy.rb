8b2b79b43d29f840b42317676691f9c419943d31
Eric Roos <ericroos13@gmail.com>
2014-11-04 10:43:55 -0600

    deploy change again

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,6 +39,7 @@ end
 # For Rails apps, we'll make some of the shared paths that are shared between
 # all releases.
 task :setup => :environment do
+  queue %[rvm rvmrc load]
   queue! %[mkdir -p "#{deploy_to}/shared/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/log"]
 
